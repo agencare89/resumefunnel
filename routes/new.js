2ed4b96793c37b79479d817cda4d6b508f72cb07
Tom Fischer <tgfischer6@gmail.com>
2015-06-23 23:40:59 -0400

    Added buttons to the new job postings page, and added a date picker

--- routes/new.js
+++ routes/new.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
 	// TODO: Replace hard-coded user with logged in user information from Passport.io (null if not logged in)
   	res.render('new', { 
   		user : 'tomfischer',
-  		'new' : 'active'
+  		'newPost' : 'active'
   	});
 });
 
