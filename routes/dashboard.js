1876da532fc64c1a901637b8cd2761d5533a8a1f
Tom Fischer <tgfischer6@gmail.com>
2015-06-25 21:54:31 -0400

    Fixing merge errors

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,19 +6,7 @@ router.get('/', function(req, res, next) {
 	// TODO: Replace hard-coded user with logged in user information from Passport.io (null if not logged in)
     // if you're on the dashboard page, you need to be logged in
   	res.render('dashboard', { 
-<<<<<<< HEAD
   		user : req.user,
-=======
-  		user : {
-  			firstName : 'Tom',
-  			lastName : 'Fischer',
-  			email : 'ftom05@ca.ibm.com',
-  			companyName : 'IBM',
-  			companyLocation : '8200 Warden Ave,\nUnionville, ON Canada\nL6G 1C7',
-  			companyWebsite : 'http://www.ibm.com/',
-  			companyLogo : 'images/IBM_logo.svg'
-  		},
->>>>>>> ef6e116219e9b4b9dbfa861066b13bce67e86d68
   		dashboard : 'active'
   	});
 });
--- routes/job.js
+++ routes/job.js
@@ -4,32 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	// TODO: Replace hard-coded user with logged in user information from Passport.io (null if not logged in)
-<<<<<<< HEAD
-    if (req.user) { 
-        res.render('job', { 
-            user: req.user
-        });
-    }
-    else { 
-        res.render('job', { 
-  		    user : null
-        }); 
-    }
-=======
-  	res.render('job', {
-  		user : null,
-  		contact : {
-  			firstName : 'Tom',
-  			lastName : 'Fischer',
-  			phoneNumber : '5195556106',
-  			email : 'ftom05@ca.ibm.com',
-  			companyName : 'IBM',
-  			companyLocation : '8200 Warden Ave,\r\nUnionville, ON Canada\r\nL6G 1C7',
-  			companyWebsite : 'http://www.ibm.com/',
-  			companyLogo : 'images/IBM_logo.svg'
-  		}
-  	});
->>>>>>> ef6e116219e9b4b9dbfa861066b13bce67e86d68
+  if (req.user) { 
+      res.render('job', { 
+          user: req.user
+      });
+  }
 });
 
 module.exports = router;
