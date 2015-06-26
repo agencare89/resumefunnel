c27c82943fb42413facef1ac4c932564509acaba
Tom Fischer <tgfischer6@gmail.com>
2015-06-25 23:16:09 -0400

    Made the datatables load data from the server

--- public/js/jobs.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('#jobPostingsTable').dataTable();
-
-	$('#jobPostingsTable tbody').on('click', 'tr', function() {
-		window.location.href = "/job/" + $('td', this).eq(0).text();
-	});
-});
