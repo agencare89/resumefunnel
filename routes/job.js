d7d01638a503d1e70731d0a14f3c45801427282f
Tom Fischer <tgfischer6@gmail.com>
2015-06-25 23:33:28 -0400

    Added table to job page, still slightly borken

--- routes/job.js
+++ routes/job.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('job', { 
-      user: req.user
+      user : req.user,
+      ownsPost : true
   });
 });
 
