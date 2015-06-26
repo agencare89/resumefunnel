751bab5ae96a3d7442e19b342d2db96d75c8cb14
Tom Fischer <tgfischer6@gmail.com>
2015-06-25 22:41:26 -0400

    Got rid of if statement blocking job posting render

--- routes/job.js
+++ routes/job.js
@@ -3,12 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	// TODO: Replace hard-coded user with logged in user information from Passport.io (null if not logged in)
-  if (req.user) { 
-      res.render('job', { 
-          user: req.user
-      });
-  }
+  res.render('job', { 
+      user: req.user
+  });
 });
 
 module.exports = router;
