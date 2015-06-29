0643ce77fab5dbac9e9bbe5830461bf4b5582d58
Tom Fischer <tgfischer6@gmail.com>
2015-06-28 22:00:38 -0400

    User must be logged in to see dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	// TODO: Replace hard-coded user with logged in user information from Passport.io (null if not logged in)
-    // if you're on the dashboard page, you need to be logged in
+router.get('/', loggedIn, function(req, res, next) {
   	res.render('dashboard', { 
   		user : req.user,
   		dashboard : 'active'
   	});
 });
+
+function loggedIn(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
 
 module.exports = router;
